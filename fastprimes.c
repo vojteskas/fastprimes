000a8c62e0a5d3debb7ed1d3ff4b7476b8cce57b
vojteskas <vojteskas99@gmail.com>
2018-09-27 20:42:31 +0200

    Update fastprimes.c

--- fastprimes.c
+++ fastprimes.c
@@ -4,16 +4,16 @@
 #include "linkedlist.c"
 					
 #define END 500								//Az do jakeho cisla ma zjistovat
-											//zda se jedna o prvocislo
+									//zda se jedna o prvocislo
 void primes(int input, node_t * head);
 void countAllPrimes(node_t * head);
 
 int main(){
-	node_t * head = newnode(2);				//Vytvori zacatek listu pro
-											//ukladani prvocisel
+	node_t * head = newnode(2);					//Vytvori zacatek listu pro
+									//ukladani prvocisel
 	
-	countAllPrimes(head);					//Najde vsechny prvocisla az
-											//do velikosti END
+	countAllPrimes(head);						//Najde vsechny prvocisla az
+									//do velikosti END
 	
 	printlist(head);						//Vypisi prvocisla
 	freelist(head);							//Uvolni pamet l. listu
@@ -32,11 +32,11 @@ void primes(int input, node_t * head){		//Zjistim zda se jedna o prvocislo nebo 
 		}
 	}
 	printf("Cislo %d je prvocislo!!\n", input);
-	append(head, input);					//Nove zjistene prvocislo pridam do linked listu
+	append(head, input);						//Nove zjistene prvocislo pridam do linked listu
 	return;
 }
 
-void countAllPrimes(node_t * head){			//Zjistuji pro vsechna cisla az do END
+void countAllPrimes(node_t * head){					//Zjistuji pro vsechna cisla az do END
 	for(int i = 3; i < END; i++){
 		primes(i, head);
 	}
