ef1ef59235e990f3096a6090dc5bb0beec43245f
Vojtěch Staněk <vojteskas99@gmail.com>
2018-10-01 22:49:25 +0200

    Included linkedlist.h instead of repeatedly typedef-ing, updated the append function to proper return values and corrected printlist function to print values on stdout.

--- linkedlist.c
+++ linkedlist.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
-typedef struct node{
-	int value;
-	struct node * next;
-}node_t;
-
-node_t * newnode(int val){						
-	node_t * new = malloc(sizeof(node_t));
-	
-	if (new == NULL){
-		fprintf(stderr, "Cannot allocate memory. Returning NULL!");
-		return NULL;
-	}
-	new->value = val;
-	new->next = NULL;
-	
-	return new;
+node_t * newnode(int val){
+    node_t * new = malloc(sizeof(node_t));
+    
+    if (new == NULL){
+        fprintf(stderr, "Cannot allocate memory. Returning NULL!");
+        return NULL;
+    }
+    new->value = val;
+    new->next = NULL;
+    
+    return new;
 }
 
-void append(node_t * current, int val){	
-	while(current->next != NULL){
-		current = current->next;
-	}
-	
-	current->next = newnode(val);
-	
-	if(current->next == NULL){
-		fprintf(stderr, "Cannot append! Exiting!\n");
-		exit(3);
-	}
+int append(node_t * current, int val){	
+    while(current->next != NULL){
+        current = current->next;
+    }
+    
+    current->next = newnode(val);
+    
+    if(current->next == NULL){
+        fprintf(stderr, "Cannot append! Exiting!\n");
+        return 1; //Returns 1 if node wasn't created
+    }
+    else {
+        return 0; //0 Errors - returns 0
+    }
 }
 
 void freelist(node_t * head){
-	while(head != NULL){
-		node_t * iterator = head->next;
-		free(head);
-		head = iterator;
-	}
+    while(head != NULL){
+        node_t * iterator = head->next;
+        free(head);
+        head = iterator;
+    }
 }
 
 void printlist(node_t * head){
-	if(head == NULL){
-		fprintf(stderr, "Pointing to null, invalid list to print.\n");
-		return;
-	}
-	
-	fprintf(stderr, "Printing linked list:\n");
-	for(;head->next != NULL; head = head->next){
-		printf("%d\n", head->value);
-	}
+    if(head == NULL){
+        fprintf(stderr, "Pointing to null, invalid list to print.\n");
+        return;
+    }
+    
+    printf("Printing linked list:\n");
+    for(;head->next != NULL; head = head->next){
+        printf("%d\n", head->value);
+    }
 }
